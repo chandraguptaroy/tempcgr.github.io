c565b9ab64ff544f0ff794a3b081458dbb88bffd
Chandra Gupta Roy <65470899+chandraguptaroy@users.noreply.github.com>
2021-07-07 19:18:42 +0530

    footer

---
+++ templates/cgrnav.js
@@ -0,0 +1,15 @@
+document.write('<header>\
+<nav>\
+    <div class="logo"><img src="images/main/cgrwhite.png" alt=""></div>\
+    <input type="checkbox" id="click">\
+    <label for="click" class="menubtn">\
+        <i class="fas fa-bars"></i>\
+    </label>\
+    <ul>\
+        <li><a class="" href="blog/blog.html">Blog</a></li>\
+        <li><a class="" href="work.html">Work</a></li>\
+        <li><a class="" href="about.html">About</a></li>\
+    </ul>\
+</nav>\
+</header>\
+    ');
