650f8efe44f3f56541de90a7e3360a3a820ad272
AKC.devel <56405323+AKCcodex@users.noreply.github.com>
2021-07-09 15:57:54 +0530

    try

--- templates/cgrnav.js
+++ templates/cgrnav.js
@@ -6,9 +6,9 @@ document.write('<header>\
         <i class="fas fa-bars"></i>\
     </label>\
     <ul>\
-        <li><a class="" href="blog/blog.html">Blog</a></li>\
-        <li><a class="" href="work.html">Work</a></li>\
-        <li><a class="" href="main/about.html">About</a></li>\
+        <li><a class="" href="/Blog/blog.html">Blog</a></li>\
+        <li><a class="" href="/main/work.html">Work</a></li>\
+        <li><a class="" href="/main/about.html">About</a></li>\
     </ul>\
 </nav>\
 </header>\
